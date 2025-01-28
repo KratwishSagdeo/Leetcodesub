39ed6bbfb054e7262b1603b34837cb94b8ab4737
kratwish sagdeo <sagdeokm@rknec.edu>
2025-01-28 09:13:23 +0530

    Time: 0 ms (100%), Space: 46 MB (7.32%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int total = (n*(n+1))/2;
+        int count = 0;
+        for(int i = 0;i<nums.length;i++){
+            count = nums[i] + count;
+        }
+        total = total-count;
+        return total;
+    }
+}
