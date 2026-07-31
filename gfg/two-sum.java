c38b41ad3c18dae737cfda570675a546eb750b6a
kratwish sagdeo <sagdeokm@rknec.edu>
2026-07-31 17:10:37 +0530

    Add solution: Two Sum

---
+++ gfg/two-sum.java
@@ -0,0 +1,17 @@
+// Problem: Two Sum
+// Link: https://www.geeksforgeeks.org/problems/key-pair5616/1
+
+class Solution {
+    boolean twoSum(int arr[], int target) {
+        // code here
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0;i<arr.length;i++){
+            int complement = target-arr[i];
+            if(set.contains(complement)){
+                return true;
+            }
+            set.add(arr[i]);
+        }
+        return false;
+    }
+}
