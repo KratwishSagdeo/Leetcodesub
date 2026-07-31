7af8a75eaae952e37f70a1852b959181672caa78
kratwish sagdeo <sagdeokm@rknec.edu>
2026-07-31 16:42:54 +0530

    Add solution: Move All Zeroes to End

---
+++ gfg/move-all-zeroes-to-end.java
@@ -0,0 +1,17 @@
+// Problem: Move All Zeroes to End
+// Link: https://www.geeksforgeeks.org/problems/move-all-zeroes-to-end-of-array0751/1
+
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        // code here
+        int count = 0;
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i]!=0){
+                arr[count++] = arr[i];
+            }
+        }
+        while(count<arr.length){
+            arr[count++] = 0;
+        }
+    }
+}
