88b689dd176d472decdd0ada8d554bd53a3916b2
kratwish sagdeo <sagdeokm@rknec.edu>
2026-08-03 16:02:51 +0530

    Update solution: CSES

--- misc/cses.java
+++ misc/cses.java
@@ -1,54 +1,21 @@
 // Problem: CSES
-// Link: https://cses.fi/problemset/result/18150336/
+// Link: https://cses.fi/problemset/result/18204661/
 
-import java.io.*;
 import java.util.*;
-
-class smaller {
-    public static void main(String args[]) throws IOException {
-
+import java.io.*;
+class Remove{
+    public static void main(String [] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int arr[] = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int fin[] = new int[n];
-
-        fin[0] = 0;
-
-        int index = 1;
-        int left = index - 1;
-
-        while (index < n) {
-
-            if (left < 0) {
-                fin[index] = 0;
-                index++;
-
-                if (index < n)
-                    left = index - 1;
-            } else {
-
-                if (arr[left] < arr[index]) {
-                    fin[index] = left + 1;   // 1-based position
-                    index++;
-
-                    if (index < n)
-                        left = index - 1;
-                } else {
-                    left--;
-                }
+        long arr[] = new long[n+1];
+        arr[0] = 1;
+        for(int i = 1;i<=n;i++){
+            for(int dice = 1;dice<=6;dice++){
+                if(i-dice>=0){
+                    arr[i] = (arr[i] + arr[i-dice]) % 1000000007;
             }
         }
-
-        for (int x : fin)
-            System.out.print(x + " ");
     }
+    System.out.println(arr[n]);
+}
 }
