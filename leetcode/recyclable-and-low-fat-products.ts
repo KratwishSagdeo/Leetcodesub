3df7f48ab23798c5aeb7f296820caf8e3c5f0d84
kratwish sagdeo <sagdeokm@rknec.edu>
2026-08-10 14:17:31 +0530

    Add solution: Recyclable and Low Fat Products

---
+++ leetcode/recyclable-and-low-fat-products.ts
@@ -0,0 +1,6 @@
+// Problem: Recyclable and Low Fat Products
+// Link: https://leetcode.com/problems/recyclable-and-low-fat-products/
+
+# Write your MySQL query statement below
+Select product_id from Products
+where low_fats = "Y" AND recyclable = "Y";
