ec3734c6165cf1a167a2bb435b421ca76cab3587
kratwish sagdeo <sagdeokm@rknec.edu>
2026-08-12 11:41:30 +0530

    Time: 30 ms (76.19%), Space: 44.5 MB (65.97%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numSquares(int n) {
+        int dp[] = new int[n+1];
+        for(int i = 0;i<=n;i++){
+            dp[i] = i;
+            for(int j = 1;j*j<=i;j++){
+                int coin = j*j;
+
+                dp[i] = Math.min(dp[i],1+dp[i-coin]);
+            }
+        }
+        return dp[n];
+    }
+}
