8e92bb86d547c09dd7e8711b45bce869703c7be6
kratwish sagdeo <sagdeokm@rknec.edu>
2026-08-14 00:30:23 +0530

    Add solution: Invalid Tweets

---
+++ leetcode/invalid-tweets.ts
@@ -0,0 +1,6 @@
+// Problem: Invalid Tweets
+// Link: https://leetcode.com/problems/invalid-tweets/?envType=study-plan-v2&envId=top-sql-50
+
+# Write your MySQL query statement below
+Select tweet_id from Tweets
+where Length(content)>15;
