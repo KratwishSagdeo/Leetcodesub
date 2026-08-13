b0d504b965618db6d8ba6e193b4b5f3003242fe7
kratwish sagdeo <sagdeokm@rknec.edu>
2026-08-14 00:42:31 +0530

    Update solution: Replace Employee ID With The Unique Identifier

--- leetcode/replace-employee-id-with-the-unique-identifier.ts
+++ leetcode/replace-employee-id-with-the-unique-identifier.ts
@@ -1,5 +1,5 @@
 // Problem: Replace Employee ID With The Unique Identifier
-// Link: https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/?envType=study-plan-v2&envId=top-sql-50
+// Link: https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/submissions/2105891176/?envType=study-plan-v2&envId=top-sql-50
 
 # Write your MySQL query statement below
 Select EmployeeUNI.unique_id,Employees.name From Employees
