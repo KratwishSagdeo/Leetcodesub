de8959d502e55334e79cc634474975ea9a12733c
kratwish sagdeo <sagdeokm@rknec.edu>
2026-08-14 00:26:55 +0530

    Add solution: Article Views I

---
+++ leetcode/article-views-i.ts
@@ -0,0 +1,7 @@
+// Problem: Article Views I
+// Link: https://leetcode.com/problems/article-views-i/?envType=study-plan-v2&envId=top-sql-50
+
+Select distinct author_id as id
+from Views
+where author_id = viewer_id
+order by id asc;
