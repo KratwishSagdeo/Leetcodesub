73307b4cd15c2aef22ad9f21007d9f85e9371eb5
kratwish sagdeo <sagdeokm@rknec.edu>
2026-08-17 22:07:46 +0530

    Add solution: Average Selling Price

---
+++ leetcode/average-selling-price.ts
@@ -0,0 +1,11 @@
+// Problem: Average Selling Price
+// Link: https://leetcode.com/problems/average-selling-price/?envType=study-plan-v2&envId=top-sql-50
+
+# Write your MySQL query statement below
+Select p.product_id, 
+ifNull(round(sum(p.price*u.units)/sum(u.units),2),0)as average_price
+from Prices p
+left join UnitsSold u
+on p.product_id = u.product_id
+and u.purchase_date between start_date and end_date
+group by p.product_id;
