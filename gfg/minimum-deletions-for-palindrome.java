0c38a542f5b414a24912c666bba6f70026c3a09a
kratwish sagdeo <sagdeokm@rknec.edu>
2026-08-29 20:10:58 +0530

    Add solution: Minimum Deletions for Palindrome

---
+++ gfg/minimum-deletions-for-palindrome.java
@@ -0,0 +1,32 @@
+// Problem: Minimum Deletions for Palindrome
+// Link: https://www.geeksforgeeks.org/problems/minimum-deletitions1648/1
+
+class Solution {
+    static int minDeletions(String s) {
+        // code here
+        int n = s.length();
+        String str = new StringBuilder(s).reverse().toString();
+        int m = str.length();
+        int dp[][] = new int[n+1][m+1];
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(i == 0){
+                    dp[i][0]= 0;
+                }
+                if(j == 0){
+                    dp[0][j] = 0;
+                }
+            }
+        }
+        for(int i = 1;i<n+1;i++){
+            for(int j = 1;j<m+1;j++){
+                if(s.charAt(i-1) == str.charAt(j-1)){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return n-dp[n][m];
+    }
+}
