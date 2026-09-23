bb577be8ad718850ea6e75739120bd884674bc26
kratwish sagdeo <sagdeokm@rknec.edu>
2026-09-23 11:01:27 +0530

    Time: 21 ms (9.47%), Space: 50 MB (21.69%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,7 +1,8 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map <String,List> map = new HashMap<>(); 
-        for(String s : strs){ // strs me multiple string hai so s
+        Map<String,List> map = new HashMap<>();
+        char ch[] = new char[26];
+        for(String s:strs){
             int count[] = new int[26];
             for(char c:s.toCharArray()){
                 count[c-'a']++;
