3bad5da6454b40f122589f12119dd0e7f00705f0
kratwish sagdeo <sagdeokm@rknec.edu>
2026-09-23 15:46:43 +0530

    Add solution: Revising the Select Query II

---
+++ hackerrank/revising-the-select-query-ii.ts
@@ -0,0 +1,4 @@
+// Problem: Revising the Select Query II
+// Link: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
+
+SELECT NAME FROM CITY WHERE COUNTRYCODE = "USA" AND POPULATION > 120000;
