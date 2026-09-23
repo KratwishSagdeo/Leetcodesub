972bc483ecd71073f39e34d1b134f36b853dca5e
kratwish sagdeo <sagdeokm@rknec.edu>
2026-09-23 15:49:29 +0530

    Add solution: Japanese Cities' Attributes

---
+++ hackerrank/japanese-cities-attributes.ts
@@ -0,0 +1,4 @@
+// Problem: Japanese Cities' Attributes
+// Link: https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true
+
+SELECT * FROM CITY WHERE COUNTRYCODE = "JPN";
