7dcee3f2a1f7fcd968e1c73f9e665ef8cecd96ae
kratwish sagdeo <sagdeokm@rknec.edu>
2026-09-23 15:57:52 +0530

    Add solution: Weather Observation Station 4

---
+++ hackerrank/weather-observation-station-4.ts
@@ -0,0 +1,4 @@
+// Problem: Weather Observation Station 4
+// Link: https://www.hackerrank.com/challenges/weather-observation-station-4/problem?isFullScreen=true
+
+SELECT COUNT(CITY)-COUNT(DISTINCT CITY) FROM STATION;
