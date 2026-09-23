acf9712419c1ec1f5d559b65a98e38efacadd405
kratwish sagdeo <sagdeokm@rknec.edu>
2026-09-23 13:37:57 +0530

    Add solution: Coin Change (Count Ways)

---
+++ gfg/coin-change-count-ways.java
@@ -0,0 +1,23 @@
+// Problem: Coin Change (Count Ways)
+// Link: https://www.geeksforgeeks.org/problems/coin-change2448/1
+
+class Solution {
+    public int count(int coins[], int sum) {
+        // code here.
+        int n = coins.length;
+        int dp[][] = new int[n+1][sum+1];
+        for(int i = 0;i<=n;i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=sum;j++){
+                if(coins[i-1]<=j){
+                    dp[i][j] = dp[i-1][j]+dp[i][j-coins[i-1]];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+}
