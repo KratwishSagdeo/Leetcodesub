69e88521bba88ca9b723af345ca51f3c2ea4d0ae
kratwish sagdeo <sagdeokm@rknec.edu>
2026-09-25 00:10:23 +0530

    Add solution: Japan Population

---
+++ hackerrank/japan-population.ts
@@ -0,0 +1,4 @@
+// Problem: Japan Population
+// Link: https://www.hackerrank.com/challenges/japan-population/problem?isFullScreen=true
+
+select sum(population) from city where countrycode = 'JPN';
