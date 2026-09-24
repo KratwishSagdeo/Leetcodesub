3a9c7f4aa42b6986353220ae20dc30e9f2903ca5
kratwish sagdeo <sagdeokm@rknec.edu>
2026-09-24 23:21:22 +0530

    Time: 5 ms (82.71%), Space: 77.4 MB (49.08%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,26 +1,18 @@
 class Solution {
     public int maxArea(int[] height) {
-        int p1 = 0, p2 = height.length - 1;
-        int maxArea = 0;
-        int area = 0;
-        int n;
-        while (p1 < p2) {
-            if(height[p1] <= height[p2]){
-                n = p2-p1;
-                area = height[p1] * n;
-                if(area > maxArea){
-                    maxArea = area;
-                }
+        int n = height.length;
+        int p1 = 0;
+        int p2 = n-1;
+        int sum = 0;
+        while(p1<p2){
+            int diff = p2-p1;
+            sum = Math.max(sum,(Math.min(height[p1],height[p2])*diff));
+            if(height[p1]<height[p2]){
                 p1++;
             }else{
-                n = p2-p1;
-                area = height[p2] * n;
-                if(area > maxArea){
-                    maxArea = area;
-                }
                 p2--;
             }
         }
-        return maxArea;
+        return sum;
     }
 }
