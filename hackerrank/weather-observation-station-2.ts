5da48aec64de973f7c03b72718b2703a9e9d4f5f
kratwish sagdeo <sagdeokm@rknec.edu>
2026-09-25 00:40:55 +0530

    Add solution: Weather Observation Station 2

---
+++ hackerrank/weather-observation-station-2.ts
@@ -0,0 +1,4 @@
+// Problem: Weather Observation Station 2
+// Link: https://www.hackerrank.com/challenges/weather-observation-station-2/problem?isFullScreen=true
+
+Select round(Sum(LAT_N),2), round(Sum(LONG_W),2) from Station; 
