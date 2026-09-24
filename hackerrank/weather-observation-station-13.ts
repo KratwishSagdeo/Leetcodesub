48a2f1db01c30c09c391c3e931a95544a7464520
kratwish sagdeo <sagdeokm@rknec.edu>
2026-09-25 00:46:29 +0530

    Add solution: Weather Observation Station 13

---
+++ hackerrank/weather-observation-station-13.ts
@@ -0,0 +1,5 @@
+// Problem: Weather Observation Station 13
+// Link: https://www.hackerrank.com/challenges/weather-observation-station-13/problem?isFullScreen=true
+
+Select round(sum(LAT_N),4) from Station
+where LAT_N > 38.7880 AND LAT_N < 137.2345;
