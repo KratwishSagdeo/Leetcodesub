b2b329b2e276f70d2d92a3373d4fc60f3be7b013
kratwish sagdeo <sagdeokm@rknec.edu>
2026-09-25 00:14:47 +0530

    Add solution: Population Density Difference

---
+++ hackerrank/population-density-difference.ts
@@ -0,0 +1,5 @@
+// Problem: Population Density Difference
+// Link: https://www.hackerrank.com/challenges/population-density-difference/problem?isFullScreen=true
+
+select Max(Population)-Min(Population)
+from city;
