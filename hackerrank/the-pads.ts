207383bf023a3821dfa1c56d0b828aaecbe9275c
kratwish sagdeo <sagdeokm@rknec.edu>
2026-09-24 23:50:59 +0530

    Add solution: The PADS

---
+++ hackerrank/the-pads.ts
@@ -0,0 +1,11 @@
+// Problem: The PADS
+// Link: https://www.hackerrank.com/challenges/the-pads/problem?isFullScreen=true
+
+SELECT CONCAT(Name, '(', SUBSTR(Occupation, 1, 1), ')')
+FROM OCCUPATIONS
+ORDER BY Name;
+
+SELECT CONCAT('There are a total of ', COUNT(Occupation), ' ', LOWER(Occupation), 's.')
+FROM OCCUPATIONS
+GROUP BY Occupation
+ORDER BY COUNT(Occupation) ASC, Occupation ASC;
