e8a64da9e616add997df373b6d7909abc79aa753
kratwish sagdeo <sagdeokm@rknec.edu>
2026-09-25 15:34:40 +0530

    Add solution: Weather Observation Station 18

---
+++ hackerrank/weather-observation-station-18.ts
@@ -0,0 +1,4 @@
+// Problem: Weather Observation Station 18
+// Link: https://www.hackerrank.com/challenges/weather-observation-station-18/problem?isFullScreen=true
+
+Select ROUND(ABS(MIN(LAT_N) - Max(LAT_N)) + ABS(MIN(LONG_W)-MAX(LONG_W)),4) from station;
