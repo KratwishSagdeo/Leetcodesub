edebc2984a33fb92bb43a59ab3e177d3db79da7f
kratwish sagdeo <sagdeokm@rknec.edu>
2026-09-25 22:23:31 +0530

    Add solution: Reverse Words in a String III

---
+++ leetcode/reverse-words-in-a-string-iii.ts
@@ -0,0 +1,30 @@
+// Problem: Reverse Words in a String III
+// Link: https://leetcode.com/problems/reverse-words-in-a-string-iii/submissions/2153186303/
+
+class Solution {
+    public String reverseWords(String s) {
+        char[] chars = s.toCharArray();
+        int start = 0;
+
+        for (int end = 0; end < chars.length; end++) {
+            if (chars[end] == ' ') {
+                reverse(chars, start, end - 1);
+                start = end + 1;
+            } else if (end == chars.length - 1) {
+                reverse(chars, start, end); // Include the last character
+            }
+        }
+
+        return new String(chars);
+    }
+
+    private void reverse(char[] chars, int left, int right) {
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
