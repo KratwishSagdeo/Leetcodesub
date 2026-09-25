6de4eef3c73a6171fe9463d4df24608d0f057bca
kratwish sagdeo <sagdeokm@rknec.edu>
2026-09-25 15:25:21 +0530

    Add solution: Weather Observation Station 15

---
+++ hackerrank/weather-observation-station-15.ts
@@ -0,0 +1,8 @@
+// Problem: Weather Observation Station 15
+// Link: https://www.hackerrank.com/challenges/weather-observation-station-15/problem?isFullScreen=true
+
+SELECT ROUND(LONG_W, 4) 
+FROM STATION 
+WHERE LAT_N < 137.2345 
+ORDER BY LAT_N DESC 
+LIMIT 1;
